494b5e6bf4742f45177049688596d03e66a5e9fe
JinxiYang <2023302111006@whu.edu.cn>
2026-06-22 16:19:56 +0800

    chore: 添加 .gitignore 忽略 build 目录和编译产物

--- build/src/parser/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jinxiyang/rmdb/src/parser
-# Build directory: /home/jinxiyang/rmdb/build/src/parser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_parser "/home/jinxiyang/rmdb/build/bin/test_parser")
-set_tests_properties(test_parser PROPERTIES  WORKING_DIRECTORY "/home/jinxiyang/rmdb/build/bin" _BACKTRACE_TRIPLES "/home/jinxiyang/rmdb/src/parser/CMakeLists.txt;14;add_test;/home/jinxiyang/rmdb/src/parser/CMakeLists.txt;0;")
