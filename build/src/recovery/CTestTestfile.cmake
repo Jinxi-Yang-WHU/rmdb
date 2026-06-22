672e970d90d50cd0ea5965aa415f15a8a10645e3
JinxiYang <2023302111006@whu.edu.cn>
2026-06-22 16:08:03 +0800

    question1

---
+++ build/src/recovery/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jinxiyang/rmdb/src/recovery
+# Build directory: /home/jinxiyang/rmdb/build/src/recovery
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/src/replacer/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jinxiyang/rmdb/src/replacer
+# Build directory: /home/jinxiyang/rmdb/build/src/replacer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
